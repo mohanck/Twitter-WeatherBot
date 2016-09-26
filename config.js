eb1754235ebc429b925d81e915cc38fc8bb59168
Mohan C <mohan.chaturvedula@gmail.com>
2016-09-26 17:03:46 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,12 +1,6 @@
-// Change this file to config.js
-// Add your keys
-// Add file .gitignore: config.js
-// Load with 
-// var config = require('./config.js');
-
 module.exports = {
-  consumer_key:         'mifeeolbxSXYHWQnlszaQYuyc',
-  consumer_secret:      'ABB6VPKXS8DfKHBkoCnfivZqCjCK4FXorr6mNfEJWwBa95CAXY',
-  access_token:         '268436941-wNgHBXNCBvUHGdRQmTnZ48i7987bvDny7Zb8aZLP',
-  access_token_secret:  'PGR8BVH1VvogGHwadhYDoKk4H6tkIPYIl7jILeHm9TAjY'
+  consumer_key:         '',
+  consumer_secret:      '',
+  access_token:         '',
+  access_token_secret:  ''
 }
